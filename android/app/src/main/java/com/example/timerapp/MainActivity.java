62d41088266ba08f1048f08658eae059c1bae7da
ShlomoV <shlomov@gmail.com>
2024-12-25 23:49:56 +0200

    added more precise app settings

--- android/app/src/main/java/com/example/timerapp/MainActivity.java
+++ android/app/src/main/java/com/example/timerapp/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.timerapp;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView timerTextView;
+    private boolean isPortrait = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +29,49 @@ public class MainActivity extends AppCompatActivity {
 
         // Implement screen always-on feature
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Initialize timerTextView
+        timerTextView = findViewById(R.id.timerTextView);
+
+        // Set up toolbar with 3-dots icon for settings
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // Open settings activity or dialog
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            isPortrait = false;
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            isPortrait = true;
+        }
+        updateTimerTextViewSize();
+    }
+
+    private void updateTimerTextViewSize() {
+        if (isPortrait) {
+            timerTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 48);
+        } else {
+            timerTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 36);
+        }
     }
 }
